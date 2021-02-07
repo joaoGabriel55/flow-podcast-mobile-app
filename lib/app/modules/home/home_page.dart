b770c2e43a8f833de160cd2e1445d08ff0d4e5da
quaresma <j.quaresmasantos_98@hotmail.com>
2021-02-07 14:45:12 -0300

    Fixed player widget

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -19,7 +19,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends ModularState<HomePage, HomeController> {
   void _loadPodcasts() {
-    controller.selectPodcast(null);
+    // controller.selectPodcast(null);
     if (controller.showOnlyFavorites)
       controller.fetchFavoritePodcasts();
     else
@@ -82,135 +82,144 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
               ],
             ),
           ),
-          child: Observer(
-            builder: (_) {
-              switch (controller.statusPodcasts) {
-                case StatusPodcast.ERROR:
-                  return Center(
-                    child: RaisedButton(
-                      color: Colors.amber,
-                      child: Text(
-                        !controller.showOnlyFavorites
-                            ? "Load podcasts again"
-                            : "Load favorite podcasts again",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () {
-                        _loadPodcasts();
-                      },
-                    ),
-                  );
-                  break;
-                case StatusPodcast.LOADING:
-                  return Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.black,
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
-                    ),
-                  );
-                  break;
-                case StatusPodcast.SUCCESS:
-                  Map<String, Podcast> _podcastMap = controller.podcasts;
-                  List<String> _podcasts = _podcastMap.keys.toList();
-                  List<String> favorites = controller.favoritePodcastsIds;
-
-                  if (_podcasts.isEmpty) {
-                    return Container(
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      alignment: Alignment.center,
-                      child: Text(
-                        "Podcasts not found...",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    );
-                  }
-
-                  return Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      NotificationListener<ScrollNotification>(
-                        // ignore: missing_return
-                        onNotification: (ScrollNotification scrollInfo) {
-                          if (scrollInfo.metrics.pixels ==
-                              scrollInfo.metrics.maxScrollExtent) {
-                            if (!controller.showOnlyFavorites) {
-                              controller.fetchPodcasts(
-                                controller.loadMoreNextParameter,
-                              );
-                            }
-                          }
-                        },
-                        child: ListView.builder(
-                          itemCount: _podcasts.length,
-                          padding: EdgeInsets.only(
-                            bottom:
-                                controller.podcastSelected != null ? 108 : 8,
+          child: Stack(
+            children: [
+              Observer(
+                builder: (_) {
+                  switch (controller.statusPodcasts) {
+                    case StatusPodcast.ERROR:
+                      return Center(
+                        child: RaisedButton(
+                          color: Colors.amber,
+                          child: Text(
+                            !controller.showOnlyFavorites
+                                ? "Load podcasts again"
+                                : "Load favorite podcasts again",
+                            style: TextStyle(color: Colors.black),
                           ),
-                          itemBuilder: (context, index) {
-                            Podcast _podcast = _podcastMap[_podcasts[index]];
-                            return InkWell(
-                              child: Observer(
-                                builder: (_) {
-                                  return PodcastCard(
-                                    thumbnail: _podcast.thumbnailUrl,
-                                    title: _podcast.title,
-                                    isFavorite: favorites.contains(_podcast.id),
-                                    addFavorite: () {
-                                      controller
-                                          .addOrRemoveFavorite(_podcast.id);
-                                      _podcast.isFavorite =
-                                          !_podcast.isFavorite;
-                                    },
+                          onPressed: () {
+                            _loadPodcasts();
+                          },
+                        ),
+                      );
+                      break;
+                    case StatusPodcast.LOADING:
+                      return Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.black,
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.amber),
+                        ),
+                      );
+                      break;
+                    case StatusPodcast.SUCCESS:
+                      Map<String, Podcast> _podcastMap = controller.podcasts;
+                      List<String> _podcasts = _podcastMap.keys.toList();
+                      List<String> favorites = controller.favoritePodcastsIds;
+
+                      if (_podcasts.isEmpty) {
+                        return Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Podcasts not found...",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        );
+                      }
+
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          NotificationListener<ScrollNotification>(
+                            // ignore: missing_return
+                            onNotification: (ScrollNotification scrollInfo) {
+                              if (scrollInfo.metrics.pixels ==
+                                  scrollInfo.metrics.maxScrollExtent) {
+                                if (!controller.showOnlyFavorites) {
+                                  controller.fetchPodcasts(
+                                    controller.loadMoreNextParameter,
                                   );
-                                },
+                                }
+                              }
+                            },
+                            child: ListView.builder(
+                              itemCount: _podcasts.length,
+                              padding: EdgeInsets.only(
+                                bottom: controller.podcastSelected != null
+                                    ? 108
+                                    : 8,
                               ),
-                              onTap: () {
-                                controller.selectPodcast(_podcast);
-                              },
-                              onLongPress: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return PodcastDescriptionDialog(
-                                      title: _podcast.title,
-                                      description: _podcast.description,
-                                      thumbnail: _podcast.thumbnailUrl,
+                              itemBuilder: (context, index) {
+                                Podcast _podcast =
+                                    _podcastMap[_podcasts[index]];
+                                return InkWell(
+                                  child: Observer(
+                                    builder: (_) {
+                                      return PodcastCard(
+                                        thumbnail: _podcast.thumbnailUrl,
+                                        title: _podcast.title,
+                                        isFavorite:
+                                            favorites.contains(_podcast.id),
+                                        addFavorite: () {
+                                          controller
+                                              .addOrRemoveFavorite(_podcast.id);
+                                          _podcast.isFavorite =
+                                              !_podcast.isFavorite;
+                                        },
+                                      );
+                                    },
+                                  ),
+                                  onTap: () {
+                                    controller.selectPodcast(_podcast);
+                                  },
+                                  onLongPress: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return PodcastDescriptionDialog(
+                                          title: _podcast.title,
+                                          description: _podcast.description,
+                                          thumbnail: _podcast.thumbnailUrl,
+                                        );
+                                      },
                                     );
                                   },
                                 );
                               },
-                            );
-                          },
-                        ),
-                      ),
-                      Observer(
-                        builder: (_) {
-                          if (controller.podcastSelected != null) {
-                            Podcast podcast = controller.podcastSelected;
-                            return PlayerWidget(
-                              podcast: podcast,
-                              isFavorite: favorites.contains(podcast.id),
-                              addFavorite: () {
-                                controller.addOrRemoveFavorite(podcast.id);
-                              },
-                              closePlayer: () {
-                                controller.selectPodcast(null);
-                              },
-                            );
-                          } else {
-                            return Divider();
-                          }
-                        },
-                      ),
-                    ],
-                  );
-                  break;
-                default:
-                  return Container();
-                  break;
-              }
-            },
+                            ),
+                          ),
+                        ],
+                      );
+                      break;
+                    default:
+                      return Container();
+                      break;
+                  }
+                },
+              ),
+              Observer(
+                builder: (_) {
+                  if (controller.podcastSelected != null) {
+                    Podcast podcast = controller.podcastSelected;
+                    List<String> favorites = controller.favoritePodcastsIds;
+                    return PlayerWidget(
+                      podcast: podcast,
+                      isFavorite: favorites.contains(podcast.id),
+                      addFavorite: () {
+                        controller.addOrRemoveFavorite(podcast.id);
+                      },
+                      closePlayer: () {
+                        controller.selectPodcast(null);
+                      },
+                    );
+                  } else {
+                    return Divider();
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
